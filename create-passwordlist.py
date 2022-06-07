3374af18e5290a7618226967556914486b6a1a0b
the server <106686224+Mester-Root@users.noreply.github.com>
2022-06-07 22:12:31 +0430

    Add files via upload

---
+++ create-passwordlist.py
@@ -0,0 +1,265 @@
+# !/usr/bin/python
+import time,sys,os
+import random
+from colorama import Fore
+import datetime
+
+time.sleep(1)
+timer = (datetime.datetime.today())
+os.system("clear")
+print(Fore.BLUE+'')
+time.sleep(1)
+print(timer)
+time.sleep(1)
+bnr=("""
+
+
+_________________
+
+.;'                        `;,
+ .;'  ,;'                 `;,  `;,    
+.;'  ,;'  ,;'     `;,  `;,  `;,   ’’
+::   ::   :    ( )   :   ::   ::                      
+':.  ':.  ':. {/_\ ,:'  ,:'  ,:'
+ ':.  ':.    /___\    ,:'  ,:'     
+  ':.       /_____\      ,:'
+           //       \\
+_________________
+
+crack password creater passwordlist""")
+for br in bnr:
+        sys.stdout.write(br)
+        sys.stdout.flush()
+        time.sleep(0.002)
+print(Fore.WHITE+'')
+#--------
+time.sleep(1)
+panel = input(F"""
+\n\n[?] create password list for:
+
+username      [1]
+page user     [2]
+website cpanel[3]
+
+enter number method ⟩>> """)
+# --- ↑ not ready ---
+time.sleep(1)
+print(Fore.YELLOW+'')
+name = input("\n\nenter name target >> ")
+time.sleep(1)
+age = input("\nenter age target >> ")
+dat = input("\ndate happy target >> ")
+time.sleep(1)
+#----- numbers ----
+pss = "1"
+print(Fore.BLUE+'')
+while True:
+    
+    number = (random.randint(1, 9))
+    number1 = (random.randint(1, 9))
+    number2 = (random.randint(1, 9))
+    number3 = (random.randint(1, 9))
+    number4 = (random.randint(1, 9))
+    number5 = (random.randint(1, 9))
+    number6 = (random.randint(1, 9))
+    number7 = (random.randint(1, 9))
+    number8 = (random.randint(1, 9))
+    number9 = (random.randint(1, 9))
+    number10 = (random.randint(1, 9))
+    number11 = (random.randint(1, 9))
+    number12 = (random.randint(1, 9))
+    number13 = (random.randint(1, 9))
+    number14 = (random.randint(1, 9))
+    number15 = (random.randint(1, 9))
+    number16 = (random.randint(1, 9))
+    number17 = (random.randint(1, 9))
+    number18 = (random.randint(1, 9))
+    number19 = (random.randint(1, 9))
+    number20 = (random.randint(1, 9))
+    number21 = (random.randint(1, 9))
+    number22 = (random.randint(1, 9))
+    number23 = (random.randint(1, 9))
+    number24 = (random.randint(1, 9))
+    number25 = (random.randint(1, 9))
+#num ------------------ 
+    num = str(number1)
+    num1 = str(number2)
+    num2 = str(number3)
+    num3 = str(number4)
+    num4 = str(number5)
+    num5 = str(number6)
+    num6 = str(number7)
+    num7 = str(number)
+    num8 = str(number8)
+    num9 = str(number10)
+    num10 = str(number11)
+    num11 = str(number12)
+    num12 = str(number9)
+    num13 = str(number13)
+    num14 = str(number14)
+    num15 = str(number15)
+    num16 = str(number16)
+    num17 = str(number17)
+    num18 = str(number18)
+    num19 = str(number19)
+    num20 = str(number20)
+    num21 = str(number21)
+    num22 = str(number22)
+    num23 = str(number23)
+    num24 = str(number24)
+    num25 = str(number25)
+#-----------------------
+    crack = (num25+num24+num23+num22+num23)
+    hack = (num21+num22+num20+num19+num18)
+#-----------------------
+    print("\n\n")
+#--------------------
+    time.sleep(0.8)
+    h = (name+age)
+    h1 = (name+age+'1')
+    h2 = (name+age+'2')
+    h3 = (name+age+'3')
+    h4 = (name+age+'4')
+    h5 = (name+age+'5')
+    h6 = (name+age+'6')
+    h7 = (name+age+'7')
+    h8 = (name+age+'8')
+    h9 = (name+age+'9')
+    h10 = (name+age+'10')
+    h11 = (name+age+'12')
+    h12 = (name+age+'123')
+    h13 = (name+age+'1234')
+    h14 = (name+age+'12345')
+    h15 = (name+age+'123456')
+    h16 = (name+age+'1234567')
+    h17 = (name+age+'12345678')
+    h18 = (name+age+'123456789')
+    h19 = (name+age+'1234567890')
+    h20 = (name+age+'12345678910')
+    h21 = (name+age+crack)
+    h22 = (name+crack)
+    h23 = (name+age+hack)
+    h24 = (name+age+dat)
+    h25 = (name+dat)
+    h26 = ("12345"+'1')
+    h27 = ("123456"+'1')
+    h28 = ("1234567"+'1')
+    h29 = ("12345678"+'1')
+    h30 = ("123456789"+'1')
+    h31 = ("1234567890"+'1')
+    h32 = ("12345678910"+'1')
+    time.sleep(0.8)
+    print(h+'\n')
+    time.sleep(0.8)
+    print(h1+'\n')
+    time.sleep(0.8)
+    print(h2+'\n')
+    time.sleep(0.8)
+    print(h3+'\n')
+    time.sleep(0.8)
+    print(h4+'\n')
+    time.sleep(0.8)
+    print(h5+'\n')
+    time.sleep(0.8)
+    print(h6+'\n')
+    time.sleep(0.8)
+    print(h7+'\n')
+    time.sleep(0.8)
+    print(h8+'\n')
+    time.sleep(0.8)
+    print(h9+'\n')
+    time.sleep(0.8)
+    print(h10+'\n')
+    time.sleep(0.8)
+    print(h11+'\n')
+    time.sleep(0.8)
+    print(h12+'\n')
+    time.sleep(0.8)
+    print(h13+'\n')
+    time.sleep(0.8)
+    print(h14+'\n')
+    time.sleep(0.8)
+    print(h15+'\n')
+    time.sleep(0.8)
+    print(h16+'\n')
+    time.sleep(0.8)
+    print(h17+'\n')
+    time.sleep(0.8)
+    print(h18+'\n')
+    time.sleep(0.8)
+    print(h19+'\n')
+    time.sleep(0.8)
+    print(h20+'\n')
+    time.sleep(0.8)
+    print(h21+'\n')
+    time.sleep(0.8)
+    print(h22+'\n')
+    time.sleep(0.8)
+    print(h23+'\n')
+    time.sleep(0.8)
+    print(h24+'\n')
+    time.sleep(0.8)
+    print(h25+'\n')
+    time.sleep(0.8)
+    print(h26+'\n')
+    time.sleep(0.8)
+    print(h27+'\n')
+    time.sleep(0.8)
+    print(h28+'\n')
+    time.sleep(0.8)
+    print(h29+'\n')
+    time.sleep(0.8)
+    print(h30+'\n')
+    time.sleep(0.8)
+    print(h31+'\n')
+    time.sleep(0.8)
+    print(h32+'\n')
+    #----------------------------  
+#-------------------
+    targete = h
+    target1 = h1
+    target2 = h2
+    target3 = h3
+    target4 = h4
+    target5 = h5
+    target6 = h6
+    target7 = h7
+    target8 = h8
+    target9 = h9
+    target10 = h10
+    target11 = h11
+    target12 = h12
+    target13 = h13
+    target14 = h14
+    target15 = h15
+    target16 = h16
+    target17 = h17
+    target18 = h18
+    target19 = h19
+    target20 = h20
+    target21 = h21
+    target22 = h22
+    target23 = h23
+    target24 = h24
+    target25 = h25
+    target26 = h26
+    target27 = h27
+    target28 = h28
+    target29 = h29
+    target30 = h30
+    target31 = h31
+    target32  = h32
+#--------------------
+    target = "password"
+    tar = target+'.txt'
+    time.sleep(1)
+    print()
+    print(Fore.GREEN+'passwordlist created: '+Fore.YELLOW+'\n[password.txt]'+Fore.RED+' - [LIST PASSWORD]')
+    print()
+    time.sleep(1)
+
+    with open(tar, 'w') as _target_:
+            _target_.write (F'''{targete}\n{target1}\n{target}\n{target3}\n{target4}\n{target5}\n{target6}\n{target7}\n{target8}\n{target9}\n{target10}\n{target11}\n{target12}\n{target13}\n{target14}\n{target15}\n{target16}\n{target17}\n{target18}\n{target19}\n{target20}\n{target21}\n{target22}\n{target23}\n{target24}\n{target25}\n{target26}\n{target27}\n{target28}\n{target29}\n{target30}\n{target31}\n{target32}''')
+
+    time.sleep(3)
+    #pss = (pss+1)
